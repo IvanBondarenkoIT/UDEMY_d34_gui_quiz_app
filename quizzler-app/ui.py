2a475eb843c6ab75a32824649fb112ef829e74a1
Ivan <ivan.bondarenko.it@gmail.com>
2023-04-18 12:10:38 +0400

    Done

--- quizzler-app/ui.py
+++ quizzler-app/ui.py
@@ -10,12 +10,25 @@ class QuizInterface:
         self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
-        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+        self.window.config(
+            padx=20,
+            pady=20,
+            bg=THEME_COLOR)
 
-        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
-        self.score_label.grid(row=0, column=1)
+        self.score_label = Label(
+            text=f"Score: 0/{len(self.quiz.question_list)}",
+            fg="white",
+            bg=THEME_COLOR)
 
-        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.score_label.grid(
+            row=0,
+            column=1)
+
+        self.canvas = Canvas(
+            width=300,
+            height=250,
+            bg="white")
+
         self.quiz_text = self.canvas.create_text(
             150,
             125,
@@ -23,14 +36,27 @@ class QuizInterface:
             text="Question",
             font=("Arial", 20, "italic",),
             fill=THEME_COLOR,)
-        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        self.canvas.grid(
+            row=1,
+            column=0,
+            columnspan=2,
+            pady=50)
 
         true_img = PhotoImage(file="images/true.png")
-        self.true_button = Button(image=true_img, highlightthickness=0, command=self.get_next_question)
+        self.true_button = Button(
+            image=true_img,
+            highlightthickness=0,
+            command=self.true_pressed)
+
         self.true_button.grid(row=2, column=0)
 
         false_img = PhotoImage(file="images/false.png")
-        self.false_button = Button(image=false_img, highlightthickness=0, command=self.get_next_question)
+        self.false_button = Button(
+            image=false_img,
+            highlightthickness=0,
+            command=self.false_pressed)
+
         self.false_button.grid(row=2, column=1)
 
         self.get_next_question()
@@ -38,6 +64,23 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.quiz_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}/{len(self.quiz.question_list)}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.quiz_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.quiz_text, text="You've reached the end of the quiz.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer('True'))
+
+    def false_pressed(self):
+        self.give_feedback(self.quiz.check_answer('False'))
+
+    def give_feedback(self, is_right):
+        self.canvas.config(bg='green' if is_right else 'red')
+        self.window.after(1000, self.get_next_question)
 
